12b7a70b6a797fd0fa3fdd68d76d9c490137909b
TBradley27 <thomas.bradley435@gmail.com>
2018-12-11 12:45:18 +0000

    fix bug to ensure hisat2 command references the index prefix and not a specific file which generates an error

--- sub_snakemake/map_reads/hisat2/map_reads.py
+++ sub_snakemake/map_reads/hisat2/map_reads.py
@@ -4,9 +4,11 @@ import os
 print(snakemake.params[0])
 print(snakemake.input[0])
 
-if len(snakemake.input) == 2:
+index_base_name = snakemake.input['index'].replace('.1.ht2','')
+
+if len(snakemake.input['reads']) == 1:
 	print('single_end')
-	os.system('hisat2 -x {} -U {} -S {} {}'.format(snakemake.input['index'],snakemake.input['reads'],snakemake.output,snakemake.params[0]))
+	os.system('hisat2 -x {} -U {} -S {} {}'.format(index_base_name,snakemake.input['reads'],snakemake.output,snakemake.params[0]))
 else:
 	print('paired_end')
-	os.system('hisat2 -x {} -1 {} -2 {} -S {} {}'.format(snakemake.input['index'],snakemake.input['reads'][0], snakemake.input['reads'][1], snakemake.output, snakemake.params[0]))
+	os.system('hisat2 -x {} -1 {} -2 {} -S {} {}'.format(index_base_name,snakemake.input['reads'][0], snakemake.input['reads'][1], snakemake.output, snakemake.params[0]))
