7dccbb4445c069a6d1164e7fdf827c1bbb2c1c0d
TBradley27 <thomas.bradley435@gmail.com>
2018-12-11 12:31:31 +0000

    alter shell scripts so that they source a function stored in another shell script

--- exe/prep_mirna_mature_alt.sh
+++ exe/prep_mirna_mature_alt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-. exe/prep_mirna_mature_alt.sh
+. exe/prep_mirna_mature_alt_function.sh
 
-prep_mirna_alt $1 $2
+prep_alt_miRNA $1 $2
--- validation/exe/prep_mirna_mature_alt.sh
+++ validation/exe/prep_mirna_mature_alt.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-awk '{print $1}' $1 |
-grep -A 1 'hsa-' |
-sed 's/--//g'    |
-sed '/^$/d'      |
-sed 'N;s/\n/ /'  |
-sed 's/>//g'     |
-awk '{ OFS="\t" }{ print $1,'9606',$1,$2}'  |
-sed 's/hsa-//g'
+. ../exe/prep_mirna_mature_alt_function.sh
+
+prep_alt_miRNA $1 $2
