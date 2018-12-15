682f3fc5b937dec29ea5ddd513afa9c992182236
TBradley27 <thomas.bradley435@gmail.com>
2018-12-15 11:54:34 +0000

    remove a few more redundant shell files in the exe subdirectory

--- exe/convert_fa_to_tsv.sh
+++
@@ -1,5 +0,0 @@
-convert_fa_to_tsv () {
-
-awk '{print $1}' $1 | sed 's/--//g' | sed '/^$/d' | sed 'N;s/\n/ /' | sed 's/>//g' | awk '{ print $1, substr($2,2,7) }' | awk '{OFS="\t"}{print $1,$2}' | grep -P "^[a-z][a-z][a-z]-(miR|let|lin)-?[0-9]{1,5}[a-z]?(-[0-9]-)?((-3|5)p)?"
-
-}
--- exe/extract_tx_and_gene_ids.sh
+++
@@ -1,1 +0,0 @@
-cat $1 | grep '>' | awk '{ print $1,"ensembl",'92',$7 }' | sed 's/\..//g' | tr ' ' '\t' | sed 's/gene_symbol://g' | sed 's/>//g'
