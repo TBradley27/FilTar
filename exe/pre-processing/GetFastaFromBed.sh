57b5bbecd37f47b8b232040e0744f4c79ee3747b
TBradley27 <thomas.bradley435@gmail.com>
2018-12-15 11:13:28 +0000

    remove all files confidently thought to be unnecessary from the exe subdirectory

--- exe/pre-processing/GetFastaFromBed.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-err() {
-  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2; exit 1
-}
-
-bedtools getfasta -name -s -fi $1 -fo $3 -bed $2
-
-#sed 's/:.*$//g' $3 > tmp"$4" && mv tmp"$4" $3
--- exe/targetscan7/convert_fasta_to_tsv2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cat $1 | sed 's/(.*//g' | tr '\n' '\t' | tr '>' '\n' | sed "s/\t/\t$2\t/1" | sed '/^$/d' 
