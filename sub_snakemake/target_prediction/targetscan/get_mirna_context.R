5c6264189abf60f22859dae2fecfb2f3a35eaa11
TBradley27 <thomas.bradley435@gmail.com>
2018-12-16 19:23:19 +0000

    fix bugs and make contextpp output much less verbose by necglecting individual contextpp contributions

--- sub_snakemake/target_prediction/targetscan/get_mirna_context.R
+++ sub_snakemake/target_prediction/targetscan/get_mirna_context.R
@@ -1,3 +1,3 @@
 test = filtar::get_mirna_context(snakemake@input$mirna_seed, snakemake@input$mature_mirnas, snakemake@wildcards$species)
 
-write_tsv(test, snakemake@output[[1]], col_names=FALSE)
+readr::write_tsv(test, snakemake@output[[1]], col_names=FALSE)
