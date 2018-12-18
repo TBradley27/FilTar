be9d4b8df3a28fedf47668765e5e9073cd56a012
TBradley27 <thomas.bradley435@gmail.com>
2018-12-18 22:16:32 +0000

    housekeeping: remove redundant files and replace with small wrappers

---
+++ sub_snakemake/canonical_targets/get_utr_lengths.R
@@ -0,0 +1,10 @@
+utr_lengths = filtar::get_utr_lengths(snakemake@input[[1]])
+
+write.table(
+        x=utr_lengths,
+        file=snakemake@output[[1]],
+        sep="\t",
+        quote=FALSE,
+        row.names=FALSE,
+        col.names=TRUE
+)
