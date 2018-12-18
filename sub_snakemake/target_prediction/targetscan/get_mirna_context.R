256c5d69e60720caf5cdcabc1f4670ed4d3f7361
TBradley27 <thomas.bradley435@gmail.com>
2018-12-18 22:27:06 +0000

    cleean miRNA processing scripts

--- sub_snakemake/target_prediction/targetscan/get_mirna_context.R
+++ sub_snakemake/target_prediction/targetscan/get_mirna_context.R
@@ -1,6 +1,3 @@
-library(tidyverse)
-library(filtar)
+test = filtar::get_mirna_context(snakemake@input$mirna_seed, snakemake@input$mature_mirnas, snakemake@wildcards$species)
 
-test = get_mirna_context(snakemake@input$mirna_seed, snakemake@input$mature_mirnas, snakemake@wildcards$species)
-
-write_tsv(test, snakemake@output[[1]], col_names=FALSE)
+readr::write_tsv(test, snakemake@output[[1]], col_names=FALSE)
--- sub_snakemake/target_prediction/targetscan/get_mirna_family.R
+++ sub_snakemake/target_prediction/targetscan/get_mirna_family.R
@@ -1,9 +1,3 @@
-library(plyr)
-library(tidyverse)
-library(filtar)
-
-print(snakemake@wildcards)
-
-mirna_seeds = get_mirna_family(snakemake@input[[1]], snakemake@wildcards$species)
+mirna_seeds = filtar::get_mirna_family(snakemake@input[[1]], snakemake@wildcards$species)
 
 write.table(mirna_seeds, snakemake@output[[1]], sep="\t", quote=FALSE, col.names=FALSE, row.names=FALSE)
