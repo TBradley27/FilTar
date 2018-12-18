d9249390abcf951ef1ca3d3447e598e46bf560b5
TBradley27 <thomas.bradley435@gmail.com>
2018-12-18 22:30:08 +0000

    target prediction related changes

--- sub_snakemake/target_prediction/targetscan/fix_ts_output.R
+++ sub_snakemake/target_prediction/targetscan/fix_ts_output.R
@@ -1,8 +1,4 @@
-library(plyr)
-library(tidyverse)
-library(filtar)
-
-ts_sites = fix_ts_output(snakemake@input[[1]])
+ts_sites = filtar::fix_ts_output(snakemake@input[[1]])
 
 write.table(ts_sites,
 file=snakemake@output[[1]],
