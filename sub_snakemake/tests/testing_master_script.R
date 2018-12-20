83b9940d634d46b9be9ea370e88cf65e78ef7d38
TBradley27 <thomas.bradley435@gmail.com>
2018-12-20 10:40:38 +0000

    clean testing subdirectory
    shuttle relevant code to an external R package

--- sub_snakemake/tests/testing_master_script.R
+++ sub_snakemake/tests/testing_master_script.R
@@ -4,7 +4,6 @@ library(testthat)
 
 test_file('test_3UTR_alignments.R')
 test_file('test_CDS_alignments.R')
-test_file('test_bedgraph_averaging.R')
 
 setwd('./gtf_to_bed')
 
@@ -13,22 +12,10 @@ test_file('test_gtf_to_bed.R')
 setwd('../targetscan')
 
 test_file('test_convert_fasta_to_ts.R')
-test_file('test_get_miRNA_families.R')
 test_file('test_prep_alt_mir.R')
 #test_file('targetscan/test_AIR_retrieval.R')
-test_file('test_get_mir_for_context.R')
 
 setwd('./main')
 
 test_file('test_fix_ts_output.R')
 test_file('test_contextpp_output.R')
-
-setwd('../../reannotate_3utrs')
-
-test_file('test_bed_reannotation.R')
-#test_file('test_creation_of_longest_bed.R')
-
-setwd('../cumulative_plots')
-
-test_file('test_get_utr_lengths.R')
-test_file('test_get_utr_sites.R')
