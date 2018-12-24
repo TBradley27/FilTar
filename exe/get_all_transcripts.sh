401aa54fc40d02e7749d2be8aff3ec2db51fde0a
TBradley27 <thomas.bradley435@gmail.com>
2018-12-24 23:54:01 +0000

    parallelise process for generating bed files for new annotations
    parallelise by chromosome as usual

--- exe/get_all_transcripts.sh
+++ exe/get_all_transcripts.sh
@@ -1,1 +1,1 @@
-cat $1 | awk '$3 == "transcript"' | awk '{print $14,$16}' | sed 's/"//g' | sed 's/;//g' | tr ' ' '.'
+cat $1 | awk '$3 == "transcript"' | awk '{OFS="\t"}{print $1,$14,$16}' | sed 's/"//g' | sed 's/;//g' | sed 's/\t/\./2' | grep -E "^$2	" | awk '{print $2}'
