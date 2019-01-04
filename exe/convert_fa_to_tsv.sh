a8f46c4f092fc2d68ea28b6e1ee09c36415cdb8a
TBradley27 <thomas.bradley435@gmail.com>
2019-01-04 22:31:00 +0000

    fix issue in last commit
    I accidentially overwritten an existing file

--- exe/convert_fa_to_tsv.sh
+++ exe/convert_fa_to_tsv.sh
@@ -1,1 +1,5 @@
-sed 's/(+)//g' $1 | sed 's/(-)//g' | sed 's/(-)//g' | tr '\n' '\t' | sed 's/>/\n/g' | sed '/^$/d' | awk -v species=$2 '{OFS="\t"}{ print $1,species,$2}' > $3
+convert_fa_to_tsv () {
+
+awk '{print $1}' $1 | sed 's/--//g' | sed '/^$/d' | sed 'N;s/\n/ /' | sed 's/>//g' | awk '{ print $1, substr($2,2,7) }' | awk '{OFS="\t"}{print $1,$2}' | grep -E "^[a-z][a-z][a-z]-(miR|let|lin)-?[0-9]{1,5}[a-z]?(-[0-9]-)?((-3|5)p)?"
+
+}
