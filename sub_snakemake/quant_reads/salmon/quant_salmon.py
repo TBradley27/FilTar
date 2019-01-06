1ca8ad940fbc2816309fb1f9737d3938e2348e1f
TBradley27 <thomas.bradley435@gmail.com>
2019-01-06 10:52:03 +0000

    remove directory tag from indexing and quant rules

--- sub_snakemake/quant_reads/salmon/quant_salmon.py
+++ sub_snakemake/quant_reads/salmon/quant_salmon.py
@@ -1,7 +1,11 @@
 import sys
 import os
 
+index = snakemake.input['index'].replace('sa.bin','') # remove file subtring to leave just the path to the index 
+
+print(index)
+
 if len(snakemake.input) == 2:
-        os.system('salmon quant -p {} -i {} -l A -r {} -o results/salmon/runs/{}/{}'.format(snakemake.threads,snakemake.input['index'],snakemake.input['reads'][0],snakemake.wildcards['species'],snakemake.wildcards['run_accession']))
+        os.system('salmon quant -p {} -i {} -l A -r {} -o results/salmon/runs/{}'.format(snakemake.threads,index,snakemake.input['reads'][0],snakemake.wildcards['accession']))
 else:
-        os.system('salmon quant -p {} -i {} -l A -1 {} -2 {} -o results/salmon/runs/{}/{}'.format(snakemake.threads,snakemake.input['index'],snakemake.input['reads'][0],snakemake.input['reads'][1],snakemake.wildcards['species'],snakemake.wildcards['run_accession']))                                                                                                                                                  
+        os.system('salmon quant -p {} -i {} -l A -1 {} -2 {} -o results/salmon/runs/{}'.format(snakemake.threads,index,snakemake.input['reads'][0],snakemake.input['reads'][1],snakemake.wildcards['accession']))                                                                                                                                                  
