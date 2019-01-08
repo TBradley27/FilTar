9f22a5e63bf79fee4f3c93bf4f1019b2d4b25e7e
TBradley27 <thomas.bradley435@gmail.com>
2019-01-08 20:01:35 +0000

    generalise script wrt species [ci skip]

--- exe/prep_mirna_mature_alt.sh
+++ exe/prep_mirna_mature_alt.sh
@@ -2,4 +2,4 @@
 
 . exe/prep_mirna_mature_alt_function.sh
 
-prep_alt_miRNA $1 $2
+prep_alt_miRNA $1 $2 $3
--- exe/prep_mirna_mature_alt_function.sh
+++ exe/prep_mirna_mature_alt_function.sh
@@ -2,21 +2,13 @@
 
 function prep_alt_miRNA () {
 
-	if [ $2 == 'hsa' ]
-		then tax_id='9606'
-	elif [ $2 == 'mmu' ]
-		then tax_id='10090'
-	else
-		tax_id='foo'
-	fi
-
 	awk '{print $1}' $1 |
 	grep -A 1 "$2-"  |
 	sed 's/--//g'    |
 	sed '/^$/d'      |
 	sed 'N;s/\n/ /'  |
 	sed 's/>//g'     |
-	awk -v tax_id="$tax_id" '{ OFS="\t" }{ print $1,tax_id,$1,$2}' |
+	awk -v tax_id="$3" '{ OFS="\t" }{ print $1,tax_id,$1,$2}' |
 	grep -E '\s[ACTGU]{18,24}$' | 
 	sed 's/$2-//g'
 }
