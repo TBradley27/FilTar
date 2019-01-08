85d2055df01ae10ef7bc4d83679e77af4c047289
TBradley27 <thomas.bradley435@gmail.com>
2019-01-08 21:00:49 +0000

    generalise R script wrt species [ci skip]

--- sub_snakemake/target_prediction/targetscan/get_mirna_context.R
+++ sub_snakemake/target_prediction/targetscan/get_mirna_context.R
@@ -1,3 +1,5 @@
-test = filtar::get_mirna_context(snakemake@input$mirna_seed, snakemake@input$mature_mirnas, snakemake@wildcards$species)
+species = snakemake@wildcards$species
+
+test = filtar::get_mirna_context(snakemake@input$mirna_seed, snakemake@input$mature_mirnas, snakemake@config$tax_ids[[species]])
 
 readr::write_tsv(test, snakemake@output[[1]], col_names=FALSE)
