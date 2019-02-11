855dcf8a94bf31845cad138c0576ba26adb4a2d1
TBradley27 <thomas.bradley435@gmail.com>
2019-02-11 14:30:55 +0000

    create log files for hisat2 mappings

--- sub_snakemake/with_reannotation/map_reads/hisat2/map_reads.py
+++ sub_snakemake/with_reannotation/map_reads/hisat2/map_reads.py
@@ -9,7 +9,7 @@ index_base_name = snakemake.input['index'].replace('.1.ht2','')
 
 if len(snakemake.input['reads']) == 1:
 	print('single_end')
-	os.system('hisat2 -x {} -p {} -U {} -S {} {}'.format(index_base_name,snakemake.threads,snakemake.input['reads'],snakemake.output,snakemake.params[0]))
+	os.system('hisat2 -x {} -p {} -U {} -S {} {} --summary-file reports/hisat2/{}.txt --new-summary'.format(index_base_name,snakemake.threads,snakemake.input['reads'],snakemake.output['sam_file'],snakemake.params[0], snakemake.wildcards['accession'],snakemake.wildcards['accession']))
 else:
 	print('paired_end')
-	os.system('hisat2 -x {} -p {} -1 {} -2 {} -S {} {}'.format(index_base_name,snakemake.threads,snakemake.input['reads'][0], snakemake.input['reads'][1], snakemake.output, snakemake.params[0]))
+	os.system('hisat2 -x {} -p {} -1 {} -2 {} -S {} {} --summary-file reports/hisat2/{}.txt --new-summary'.format(index_base_name,snakemake.threads,snakemake.input['reads'][0], snakemake.input['reads'][1], snakemake.output['sam_file'], snakemake.params[0], snakemake.wildcards['accession'],snakemake.wildcards['accession']))
