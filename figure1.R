a66019fefd2ff13f8050e0c1f7f05f71d3c4dc37
TBradley27 <thomas.bradley435@gmail.com>
2019-03-05 18:29:28 +0000

    generate multiple manuscript images

--- figure1.R
+++ figure1.R
@@ -3,16 +3,15 @@ library(grid)
 library(ggplot2)
 
 HeLa = readRDS(file = "results/plots/hsa_PRJNA512378_let-7c-5p_HeLa_exp.rds")
-HBE4o = readRDS(file = "results/plots/hsa_PRJNA304643_miR-1343-3p_16HBE14o_exp.rds")
 A549 = readRDS(file = "results/plots/hsa_PRJNA304643_miR-1343-3p_A549_exp.rds")
 U343 = readRDS(file='results/plots/hsa_PRJNA231155_miR-137-3p_U343_exp.rds')
-U251 = readRDS(file='results/plots/hsa_PRJNA231155_miR-137-3p_U251_exp.rds')
-Huh7 = readRDS(file='results/plots/hsa_PRJNA229375_miR-124-3p_Huh7_exp.rds')
+CD4 = readRDS(file='results/plots/mmu_PRJNA309441_miR-23a-3p_CD4_exp.rds')
+NMuMG = readRDS(file='results/plots/mmu_PRJNA340017_miR-200b-3p_NMuMG_exp.rds')
 ESCs = readRDS(file='results/plots/mmu_PRJNA270999_miR-294-3p_ESCs_exp.rds')
 
-png('foo.png', width=900, height=900)
+png('results/plots/figure_1.png', width=900, height=900)
 
-grid.arrange(HeLa, A549, U343, U251, Huh7, ESCs,
+grid.arrange(HeLa, A549, U343, CD4, NMuMG, ESCs,
              ncol=2, nrow=3,
              bottom=textGrob(
                expression(bold('log'[2]*'(mRNA Fold Change)'))
