2342d10aeacfc08fe7b587c323fed6215a33a1e5
TBradley27 <thomas.bradley435@gmail.com>
2019-03-15 19:43:56 +0000

    add rules for generating summary stats

---
+++ sub_snakemake/cumulative_plots/get_reannotation_stats.R
@@ -0,0 +1,79 @@
+library(tidyverse)
+library(tximport)
+
+original = read_tsv(snakemake@input[['canonical']])
+new = read_tsv(snakemake@input[['new']])
+pc_transcripts = read_tsv(snakemake@input[['pc_transcripts']], col_names='tx_id')
+
+merged = merge(original,new, by = 'tx_id') %>% as_tibble()
+merged = merged[merged$tx_id %in% pc_transcripts$tx_id,]
+
+merged$diff = merged$utr_length.y - merged$utr_length.x
+
+
+####
+real = strsplit(snakemake@input$quant, split='/')
+real_accessions = c()
+
+for (i in 1:length(real)) {
+  real_accessions = c(real_accessions, real[[i]][3])
+}
+
+files = c(snakemake@input$quant)
+names(files) = real_accessions
+
+txi <- tximport(files, type="kallisto", txOut=TRUE)
+
+print('foo')
+
+TPMs = as.data.frame(txi$abundance)
+TPMs$average = rowMeans(TPMs)
+TPMs$names = rownames(txi$abundance)
+
+TPMs = dplyr::filter(TPMs, average >= 5.0)
+
+merged_filtered = merged[merged$tx_id %in% TPMs$names,]
+
+print(merged)
+print(merged_filtered)
+
+###
+
+num_new_bases = sum(merged$diff[merged$diff>0])
+num_old_bases = sum(merged_filtered$diff[merged_filtered$diff<0]) %>% abs()
+
+# reannotation integer vector
+
+merged$int_diff = merged$diff
+merged_filtered$int_diff = merged_filtered$diff
+
+merged = merged %>% mutate(int_diff = replace(int_diff, int_diff > 0, 1))
+merged_filtered = merged_filtered %>% mutate(int_diff = replace(int_diff, int_diff < 0, -1))
+
+num_tx_elongated = sum(merged$int_diff[merged$int_diff>0])
+num_tx_truncated = sum(merged_filtered$int_diff[merged_filtered$int_diff<0]) %>% abs()
+
+percent_bases_plus = ( num_new_bases / sum(merged$utr_length.x) ) * 100
+percent_bases_minus = ( num_old_bases / sum(merged$utr_length.x) ) * 100 %>% abs()
+
+percent_tx_elongated = ( num_tx_elongated / length(merged$tx_id ) ) * 100
+percent_tx_truncated = ( num_tx_truncated / length(merged$tx_id ) ) * 100
+
+data = tibble(sample=snakemake@wildcards$tissue, 
+                num_new_bases = num_new_bases, num_old_bases = num_old_bases, 
+                percent_bases_gained = signif(percent_bases_plus,3), percent_bases_lost = signif(percent_bases_minus,3), 
+                num_tx_elongated = num_tx_elongated, num_tx_truncated = num_tx_truncated,
+                percent_tx_elongated = signif(percent_tx_elongated,3), percent_tx_truncated = signif(percent_tx_truncated,3)
+                )
+
+write.table(
+	x=data,
+	file=snakemake@output[[1]],
+	row.names=FALSE,
+	quote=FALSE,
+	sep='\t'
+)
+
+
+
+
---
+++ sub_snakemake/cumulative_plots/get_stats.R
@@ -0,0 +1,87 @@
+library(tidyverse)
+library(tximport)
+
+###
+
+real = strsplit(snakemake@input$quant, split='/')
+real_accessions = c()
+
+for (i in 1:length(real)) {
+  real_accessions = c(real_accessions, real[[i]][3])
+}
+
+files = c(snakemake@input$quant)
+names(files) = real_accessions
+
+txi <- tximport(files, type="kallisto", txOut=TRUE)
+
+print('foo')
+
+TPMs = as.data.frame(txi$abundance)
+TPMs$average = rowMeans(TPMs)
+TPMs$names = rownames(txi$abundance)
+
+high_expression = dplyr::filter(TPMs, average >= 5.0)
+
+print(high_expression)
+
+#high_expression = dplyr::filter(kallisto, tpm >= 5.0)
+
+###
+
+cerebellum_targets = read_tsv(snakemake@input[['tissue']])
+cerebellum_targets_names = cerebellum_targets$a_Gene_ID %>% unique()
+
+pc_transcripts = read_tsv(
+  'results/bed/hsa_all_protein_coding_transcripts.txt',
+  col_names=FALSE
+  )
+
+cerebellum_targets_names = 
+  cerebellum_targets_names[cerebellum_targets_names %in% pc_transcripts$X1]
+
+canonical_targets = read_tsv(snakemake@input[['control']])
+canonical_targets_names = canonical_targets$a_Gene_ID %>% unique()
+
+canonical_targets_names = canonical_targets_names[canonical_targets_names %in% pc_transcripts$X1]
+
+# gained target transcripts
+
+a=cerebellum_targets_names[!cerebellum_targets_names %in% canonical_targets_names] %>% length() #%>% print()
+
+# lost target transcripts
+
+old_targets=canonical_targets_names[!canonical_targets_names %in% cerebellum_targets_names]
+
+b=old_targets[old_targets %in% high_expression$names] %>% length() #%>% print()
+
+
+cerebellum_targets$Group_num = NULL
+canonical_targets$Group_num = NULL
+
+# gained target sites
+
+c=setdiff(cerebellum_targets,canonical_targets) #%>% print()
+c=dim(c)[1]
+
+# lost target sites
+
+old_target_sites=setdiff(canonical_targets,cerebellum_targets)
+
+old_target_sites = old_target_sites[old_target_sites$a_Gene_ID %in% high_expression$names,]
+
+d=dim(old_target_sites)[1] #%>% print()
+
+output = tibble(new_transcripts=a,old_transcripts=b,new_target_sites=c,old_target_sites=d)
+
+print(output)
+
+write.table(
+	output,
+	snakemake@output[[1]],
+	quote=FALSE,
+	sep="\t",
+	row.names=FALSE
+)
+
+
