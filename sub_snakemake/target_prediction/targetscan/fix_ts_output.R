06c3d12d19c6eeda12988ca9f89598ddbcae34ab
TBradley27 <thomas.bradley435@gmail.com>
2019-03-16 12:37:15 +0000

    modify how some dependencies are invoked

--- sub_snakemake/target_prediction/targetscan/fix_ts_output.R
+++ sub_snakemake/target_prediction/targetscan/fix_ts_output.R
@@ -1,7 +1,16 @@
 ts_sites = filtar::fix_ts_output(snakemake@input[[1]])
 
-write.table(ts_sites,
-file=snakemake@output[[1]],
-sep="\t",
-row.names=FALSE,
-quote=FALSE)
+if ( num_lines  == 1 ) { # test if the file contains any records 
+
+	file.copy(from=snakemake@input[[1]], to=snakemake@output[[1]])
+
+} else {
+
+	ts_sites = filtar::fix_ts_output(snakemake@input[[1]])
+
+	write.table(ts_sites,
+	file=snakemake@output[[1]],
+	sep="\t",
+	row.names=FALSE,
+	quote=FALSE)
+}
