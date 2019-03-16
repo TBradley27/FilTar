c12ef17a0247c0c9b780fbef8c299d547d46d94b
TBradley27 <thomas.bradley435@gmail.com>
2019-03-16 12:38:39 +0000

    Carry over changes from the validation branch

--- sub_snakemake/quant_reads/salmon/quant_salmon.py
+++ sub_snakemake/quant_reads/salmon/quant_salmon.py
@@ -2,6 +2,6 @@ import sys
 import os
 
 if len(snakemake.input) == 2:
-        os.system('salmon quant --validateMappings --rangeFactorizationBins 4 --seqBias --posBias --gcBuas -p {} -i {} -l A -r {} -o {}'.format(snakemake.threads,snakemake.input['index'],snakemake.input['reads'][0],snakemake.wildcards['species'],snakemake.wildcards['run_accession'],snakemake.output))
+        os.system('salmon quant --validateMappings --rangeFactorizationBins 4 --seqBias --posBias -p {} -i {} -l A -r {} -o {}'.format(snakemake.threads,snakemake.input['index'],snakemake.input['reads'][0],snakemake.output))
 else:
-        os.system('salmon quant --validateMappings --rangeFactorizationBins 4 --seqBias --posBias --gcBias -p {} -i {} -l A -1 {} -2 {} -o {}'.format(snakemake.threads,snakemake.input['index'],snakemake.input['reads'][0],snakemake.input['reads'][1],snakemake.wildcards['species'],snakemake.wildcards['run_accession'], snakemake.output))                                                                                                                                                  
+        os.system('salmon quant --validateMappings --rangeFactorizationBins 4 --seqBias --posBias -p {} -i {} -l A -1 {} -2 {} -o {}'.format(snakemake.threads,snakemake.input['index'],snakemake.input['reads'][0],snakemake.input['reads'][1],snakemake.output))     
