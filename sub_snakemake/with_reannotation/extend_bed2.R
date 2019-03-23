19e857e290749751405218630bb1d80b6068de14
TBradley27 <thomas.bradley435@gmail.com>
2019-03-23 21:07:05 +0000

    filter away lowly expressed truncations from lowly expressed transcripts

--- sub_snakemake/with_reannotation/extend_bed2.R
+++ sub_snakemake/with_reannotation/extend_bed2.R
@@ -3,7 +3,7 @@
 if ( file.info(snakemake@input$extended_bed)$size == 0 ) {
 	file.copy(from=snakemake@input$normal_bed, to=snakemake@output[[1]])	
 } else {
-	full_set_sorted = filtar::get_full_bed(snakemake@input$normal_bed, snakemake@input$extended_bed, snakemake@input$all_transcripts)
+	full_set_sorted = filtar::get_full_bed(snakemake@input$normal_bed, snakemake@input$extended_bed, snakemake@input$all_transcripts, snakemake@input$tx_quant)
         write.table(full_set_sorted, file=snakemake@output[[1]], quote=FALSE, row.names=FALSE, col.names=FALSE, sep="\t")
 
 }
