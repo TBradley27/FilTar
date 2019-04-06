2cc4bef2439378059607f70036f6329d1d0a88dc
TBradley27 <thomas.bradley435@gmail.com>
2019-04-06 10:43:55 +0100

    use TPM threshold as argument to R filtering functions

--- modules/without_reannotation/filter_miRanda_scores.R
+++ modules/without_reannotation/filter_miRanda_scores.R
@@ -2,7 +2,7 @@ if ( file.info(snakemake@input[['miRanda_scores']])$size == 0  ) {
 	file.create(snakemake@output[[1]])	
 } else {
 
-	filtered_miRanda_scores = filtar::filter_miRanda_scores(snakemake@input[['miRanda_scores']], snakemake@input[['expression_values']])
+	filtered_miRanda_scores = filtar::filter_miRanda_scores(snakemake@input[['miRanda_scores']], snakemake@input[['expression_values']],snakemake@params[['tpm_expression_threshold']])
 
 	write.table(filtered_miRanda_scores, snakemake@output[[1]], row.names=FALSE, col.names=FALSE, sep="\t", quote=FALSE)
 }
