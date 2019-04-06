5ba6a2bb77a236cce19fb2528bc674577954be12
TBradley27 <thomas.bradley435@gmail.com>
2019-04-06 10:46:09 +0100

    permit empty fasta files for merge_fasta.R

--- modules/get_utr_and_cds/without_conservation/merge_fasta.R
+++ modules/get_utr_and_cds/without_conservation/merge_fasta.R
@@ -1,4 +1,8 @@
 #!/usr/bin/env Rscript
 
-output = filtar::MergeFasta(snakemake@input[[1]])
-write.table(output, snakemake@output[[1]], quote=FALSE, sep="\t", col.names=FALSE, row.names=FALSE)
+if ( file.info(snakemake@input[[1]])$size == 0 ) {
+        file.copy(from=snakemake@input[[1]], to=snakemake@output[[1]])
+} else {
+	output = filtar::MergeFasta(snakemake@input[[1]])
+	write.table(output, snakemake@output[[1]], quote=FALSE, sep="\t", col.names=FALSE, row.names=FALSE)
+}
