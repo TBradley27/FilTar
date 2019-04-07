a39e3f85d528dbdc65e7889c8787f89e651248ed
TBradley27 <thomas.bradley435@gmail.com>
2019-04-07 12:57:14 +0100

    add a header to miRanda output

--- modules/target_prediction/miRanda/with_reannotation/filter_miRanda_scores.R
+++ modules/target_prediction/miRanda/with_reannotation/filter_miRanda_scores.R
@@ -3,6 +3,5 @@ if ( file.info(snakemake@input[['miRanda_scores']])$size == 0  ) {
 } else {
 
 	filtered_miRanda_scores = filtar::filter_miRanda_scores(snakemake@input[['miRanda_scores']], snakemake@input[['expression_values']],snakemake@params[['tpm_expression_threshold']])
-
-	write.table(filtered_miRanda_scores, snakemake@output[[1]], row.names=FALSE, col.names=FALSE, sep="\t", quote=FALSE)
+	write.table(filtered_miRanda_scores, snakemake@output[[1]], row.names=FALSE, col.names=TRUE, sep="\t", quote=FALSE)
 }
