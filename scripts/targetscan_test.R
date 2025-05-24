399b211efcf943971ea3c301a223be7250209602
Thomas Bradley <TBradley27@users.noreply.github.com>
2025-05-24 14:32:48 +0000

    fix transcript version in test

--- scripts/targetscan_test.R
+++ scripts/targetscan_test.R
@@ -8,5 +8,5 @@ print(output_mmu)
 context('targetscan')
 
 test_that('test filtar targetscan values match those on the official targetscan website', {
-        expect_equal(output_mmu$`context++ score`[output_mmu$`Gene ID` == 'ENSMUST00000189888.6'],-0.25,0.025)}
+        expect_equal(output_mmu$`context++ score`[output_mmu$`Gene ID` == 'ENSMUST00000189888.7'],-0.25,0.025)}
 )
